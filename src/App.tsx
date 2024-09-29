5f0476ae26da48773754b0c041c715e951047714
khakisage <jongjin201@gmail.com>
2024-09-29 20:44:45 +0900

    기능 구현 - 무한스크롤

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,88 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { MOCK_DATA } from './mockdata/mockdata'
+import { IMockData } from './interface/mockdata'
+import { useCallback, useEffect, useRef, useState } from 'react'
+import { Card } from './components/Card'
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+
+const PER_PAGE = 10
+
+const getMockData = (pageNum: number): Promise<{ datas: IMockData[]; isEnd: boolean }> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const datas: IMockData[] = MOCK_DATA.slice(PER_PAGE * pageNum, PER_PAGE * (pageNum + 1))
+      const isEnd = PER_PAGE * (pageNum + 1) >= MOCK_DATA.length
+
+      resolve({ datas, isEnd })
+    }, 1500)
+  })
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState<IMockData[]>([])
+  const [pageNum, setPageNum] = useState<number>(0)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [isEnd, setIsEnd] = useState<boolean>(false)
+  const [totalPrice, setTotalPrice] = useState<number>(0)
+  const observerRef = useRef<IntersectionObserver | null>(null)
+  const loadRef = useRef<HTMLDivElement | null>(null)
+
+  const sumTotalPrice = (data: IMockData[]) => {
+    return data.reduce((sum, item) => sum + item.price, 0)
+  }
+
+  const loadData = useCallback(() => {
+    if (!isEnd && !isLoading) {
+      setIsLoading(true)
+      getMockData(pageNum).then(({ datas, isEnd }) => {
+        setData((prevData) => {
+          const newData = [...prevData, ...datas]
+          setTotalPrice(sumTotalPrice(newData))
+          return newData
+        })
+        setIsEnd(isEnd)
+        setPageNum((prevPageNum) => prevPageNum + 1)
+        setIsLoading(false)
+      })
+    }
+  }, [pageNum, isEnd, isLoading])
+
+  useEffect(() => {
+    if (observerRef.current) observerRef.current.disconnect()
+
+    observerRef.current = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        loadData()
+      }
+    })
+
+    if (loadRef.current) observerRef.current.observe(loadRef.current)
+
+    return () => {
+      if (observerRef.current) observerRef.current.disconnect()
+    }
+  }, [loadData])
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>total price: {totalPrice}</h1>
+        <div className="mt-10">
+          {data.map((items) => (
+            <Card productId={items.productId} productName={items.productName} price={items.price} boughtDate={items.boughtDate} />
+          ))}
+          {isLoading && (
+            <div>
+              <SkeletonTheme baseColor="#585858" height={50}>
+                <Skeleton count={PER_PAGE} />
+              </SkeletonTheme>
+            </div>
+          )}
+          <div ref={loadRef} />
+        </div>
+        {isEnd && <div>더 이상 데이터가 없습니다.</div>}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
